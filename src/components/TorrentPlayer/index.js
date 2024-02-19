2ac9a81d8ab8d9fe889c96fff4298861df1584ef
Christopher Frick <christopher.frick@haag-streit.com>
2024-02-19 15:52:49 +0100

    debug magnet link

--- src/components/TorrentPlayer/index.js
+++ src/components/TorrentPlayer/index.js
@@ -7,18 +7,24 @@ function TorrentPlayer ({ magnetUrl, onClose }) {
     if (window.WebTorrent) {
       console.log('WebTorrent est disponible');
       const client = new window.WebTorrent();
-      const torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
-      
-      
+      const torrentId = magnetUrl;
+      const demoTorrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+      console.log('Téléchargement du torrent:', demoTorrentId);
+      console.log('magnetUrl:', torrentId);
 
       client.add(torrentId, function (torrent) {
-        // Torrents can contain many files. Let's use the .mp4 file
+        console.log('Torrent File:', torrent.files);
         const file = torrent.files.find(function (file) {
-          return file.name.endsWith('.mp4')
+          
+          return (file.name.endsWith('.mp4') ||
+                  file.name.endsWith('.mkv') ||
+                  file.name.endsWith('.avi') ||
+                  file.name.endsWith('.webm') 
+          );
         })
 
         // Display the file by adding it to the DOM. Supports video, audio, image, etc. files
-        file.appendTo('#torrent')
+        file.appendTo('#display')
       })
 
 
@@ -43,7 +49,7 @@ function TorrentPlayer ({ magnetUrl, onClose }) {
 
   return (
     <div id="torrentPlayer">
-      <div id="torrent"></div>
+      <div id="display"></div>
       <button onClick={onClose}>Fermer</button>
     </div>
   );
